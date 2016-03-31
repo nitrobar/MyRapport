8e8df6937956b7494f0e462fc7d70eda2e537fb2
student <student@ubuntu>
2016-03-31 14:12:31 +0200

    Second commit

--- web/my_project/src/AppBundle/Controller/DefaultController.php
+++ web/my_project/src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Entity\Kundenliste;
+use Symfony\Component\HttpFoundation\Response;
+
 class DefaultController extends Controller
 {
     /**
@@ -17,5 +20,24 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+    
+    public function createAction()
+    {
+    	$kundenliste = new Kundenliste();
+    	$kundenliste->setName('Max Muster');
+    	$kundenliste->setAdresse('Musterstrasse 12');
+    	$kundenliste->setOrt('Chur');
+    	$kundenliste->setTelefon('0813539988');
+    	
+    	$em = $this->getDoctrine()->getManager();
+   
+    	// tells Doctrine you want to (eventually) save the Product (no queries yet)
+    	$em->persist($kundenliste);
+    
+    	// actually executes the queries (i.e. the INSERT query)
+    	$em->flush();
+    
+    	return new Response('Saved new product with id '.$kundenliste->getId());
     }
 }
