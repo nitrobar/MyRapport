6402a481a29065502e5a80a80f87de08a00c9a40
student <nino@drc.ch>
2016-04-01 14:48:53 +0200

    MyRapport Bundle

--- web/my_project/app/AppKernel.php
+++ web/my_project/app/AppKernel.php
@@ -17,7 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+        	new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         	new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+            new Acme\MyRapportBundle\AcmeMyRapportBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
