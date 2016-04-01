4ee332fd75e9ae44c9717002e326a54f5c440503
Nino Sargenti <nino@drc.ch>
2016-04-01 16:55:09 +0200

    Twig Ergänzungen

--- web/my_project/app/AppKernel.php
+++ web/my_project/app/AppKernel.php
@@ -19,7 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
         	new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         	new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
-            new Acme\MyRapportBundle\AcmeMyRapportBundle(),
+            
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- web/my_project/src/Acme/MyRapportBundle/AcmeMyRapportBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Acme\MyRapportBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AcmeMyRapportBundle extends Bundle
-{
-}
